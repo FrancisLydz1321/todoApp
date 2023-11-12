82ee29cfa36389c0cd371cddd1d2cbd98b7c1df0
Francis Lydzustre <francislydzustre1321@gmail.com>
2023-11-13 00:48:42 +0800

    Second Commit0

--- lib/todo_service.dart
+++ lib/todo_service.dart
@@ -4,8 +4,7 @@ import 'package:todo_app/todo_item.dart';
 class TodoService {
   final String _boxName = "todoBox";
 
-  Future<Box<TodoItem>> get _box async =>
-      await Hive.openBox<TodoItem>(_boxName);
+  Future<Box<TodoItem>> get _box async => enBox<TodoItem>(_boxName);
 
   Future<void> addItem(TodoItem todoItem) async {
     var box = await _box;
